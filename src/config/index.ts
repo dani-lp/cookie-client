c82c3a736308afde2358e79a6163ef6df5b65d19
dani-lp <danilau11@gmail.com>
2022-07-27 23:49:22 +0200

    Migrated from CRA to Vite

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL as string;
+export const API_URL = import.meta.env.VITE_API_URL as string;
 export const DEFAULT_RECIPE_IMG_URL = 'https://i.ytimg.com/vi/EoufLVAAPSU/maxresdefault.jpg';
